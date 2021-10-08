9fd5b7222901309e4549e4d2a78c517d9b1867bf
Aqilahabd <aalmohse@emich.edu>
2021-10-08 00:11:53 +0000

    Added

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <div>
+        <header>
+
+          <p>
+         Aqilah Almohsen
+          </p>
+      
+        </header>
+      </div>
+    );
+  }
+  
+  export default Header;
